d78973db6a04deb77c47d21b01780633c3451dfc
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-11-26 16:47:46 +0100

    Created model for follower and following users list

---
+++ model/user_models/get_following_user_list.php
@@ -0,0 +1,29 @@
+<?php
+include '../login_models/login_functions.php';
+require_once("../connection_models/db_conn.php");
+
+$query = "SELECT U.Email, U.Username, U.FotoProfilo
+        FROM utenti U INNER JOIN seguiti S ON U.Email = S.EmailSeguito
+        WHERE S.EmailFollower = ?";
+
+session_start();
+if (checkLogin($conn)) {
+    if ($selectQuery = $conn->prepare($query)) {
+        $selectQuery->bind_param("s", $_SESSION['userEmail']);
+        if ($selectQuery->execute()) {
+            $results = $selectQuery->get_result();
+            $temp = $results->fetch_all(MYSQLI_ASSOC);
+            $temp[0]['FotoProfilo'] = base64_encode($temp[0]['FotoProfilo']);
+            echo json_encode($temp);
+        } else {
+            echo json_encode(array("error" => $selectQuery->error));
+        }
+    } else {
+        echo json_encode(array("error" => $selectQuery->error));
+    }
+} else {
+    echo json_encode(array("error" => "The user is not logged"));
+}
+
+$conn->close();
+?>
