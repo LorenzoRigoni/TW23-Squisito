9bc6ae7f5a6c11901abe8f11a9f79af0e938f0ad
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-11-29 15:23:52 +0100

    Changed data to send to follow models

--- model/user_models/follow_models.php
+++ model/user_models/follow_models.php
@@ -1,14 +1,15 @@
 <?php
 include '../login_models/login_functions.php';
-require('../connection_models/db_conn.php');
 
 session_start();
 if (checkLogin($conn)) {
+    $followingEmail = getEmailFollowing();
+    require('../connection_models/db_conn.php');
     $query = "SELECT *
         FROM seguiti S
         WHERE S.EmailSeguito = ? AND S.EmailFollower = ?";
     if ($isFollowed = $conn->prepare($query)) {
-        $isFollowed->bind_param("ss", $_GET['emailSeguito'], $_SESSION['userEmail'], );
+        $isFollowed->bind_param("ss", $followingEmail, $_SESSION['userEmail'], );
         if ($isFollowed->execute()) {
             $query = "";
             if ($isFollowed->get_result()->num_rows == 0) {
@@ -19,7 +20,7 @@ if (checkLogin($conn)) {
                         WHERE EmailFollower = ? AND EmailSeguito = ?";
             }
             $conn->close();
-            $result = executeQuery($query);
+            $result = executeQuery($query, $followingEmail);
             echo json_encode($result);
         } else {
             echo json_encode(array("error" => $isFollowed->error));
@@ -34,12 +35,13 @@ if (checkLogin($conn)) {
 /**
  * Function for execute the SQL queries.
  * @param string $query The query to execute
+ * @param string $followingEmail The email ot the following user
  * @return array An associative array with the results
  */
-function executeQuery($query) {
+function executeQuery($query, $followingEmail) {
     require('../connection_models/db_conn.php');
     if ($selectQuery = $conn->prepare($query)) {
-        $selectQuery->bind_param("ss", $_SESSION['userEmail'], $_GET['emailSeguito']);
+        $selectQuery->bind_param("ss", $_SESSION['userEmail'], $followingEmail);
         if ($selectQuery->execute()) {
             if (explode(" ", $query)[0] == "INSERT") {
                 return array("alreadyFollowed" => false);
@@ -51,6 +53,28 @@ function executeQuery($query) {
         }
     } else {
         return array("error" => $selectQuery->error);
+    }
+    $conn->close();
+}
+
+/**
+ * Function for getting the email of the user who created a post.
+ * @return string The email of the user.
+ */
+function getEmailFollowing() {
+    require('../connection_models/db_conn.php');
+    $query = "SELECT P.EmailUtente
+            FROM post P
+            WHERE P.IDPost = ?";
+    if ($selectQuery = $conn->prepare($query)) {
+        $selectQuery->bind_param("i", $_GET['IDPost']);
+        if ($selectQuery->execute()) {
+            return $selectQuery->get_result()->fetch_assoc()['IDPost'];
+        } else {
+            return $selectQuery->error;
+        }
+    } else {
+        return $selectQuery->error;
     }
     $conn->close();
 }
