887e446f03f2a5f7d31cb2d7012f62aecadc9cfd
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-11-30 14:34:03 +0100

    Fixed error in logout model

--- model/logout_models/logout.php
+++ model/logout_models/logout.php
@@ -2,10 +2,9 @@
 include '../login_models/login_functions.php';
 include '../session_models/create_sec_session.php';
 
-start_session();
+session_start();
 $_SESSION = array();
 $params = session_get_cookie_params();
 setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 session_destroy();
-header('Location: ./');
 ?>
