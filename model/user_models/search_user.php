86f7c9954a3652711bac9338931f6082cadb82e8
Niccolò Bertozzi <116648770+NiccoloBert0zzi@users.noreply.github.com>
2023-12-01 14:46:35 +0100

    Revert "search"
    This reverts commit a852de9964acfaee6f20aea8bb7a290ab5af5350.

--- model/user_models/search_user.php
+++ model/user_models/search_user.php
@@ -4,20 +4,17 @@ require_once("../connection_models/db_conn.php");
 
 $query = "SELECT U.Username, U.FotoProfilo
         FROM utenti U 
-        WHERE  U.Username LIKE ? AND U.Email != ?";
+        WHERE  U.Username LIKE '?%' AND U.Email != ?";
 
 session_start();
 if (checkLogin($conn)) {
     if ($selectQuery = $conn->prepare($query)) {
-        $param = $_GET['name'] . '%';
-        $selectQuery->bind_param("ss", $param, $_SESSION['userEmail']);
+        $selectQuery->bind_param("ss", $_GET['name'], $_SESSION['userEmail']);
         if ($selectQuery->execute()) {
             $results = $selectQuery->get_result();
-            if ($results->num_rows > 0) {
-                $temp = $results->fetch_all(MYSQLI_ASSOC);
-                $temp[0]['FotoProfilo'] = base64_encode($temp[0]['FotoProfilo']);
-                echo json_encode($temp);
-            }
+            $temp = $results->fetch_all(MYSQLI_ASSOC);
+            $temp[0]['FotoProfilo'] = base64_encode($temp[0]['FotoProfilo']);
+            echo json_encode($temp);
         } else {
             echo json_encode(array("error" => $selectQuery->error));
         }
