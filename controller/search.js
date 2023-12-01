8b93bb8d8bcd95372b7ccc6ffbf7ac784c9eb8d3
Niccolò Bertozzi <116648770+NiccoloBert0zzi@users.noreply.github.com>
2023-12-01 15:03:05 +0100

    search

--- controller/search.js
+++ controller/search.js
@@ -27,33 +27,35 @@ window.addEventListener("load", function () {
   function explore() {
     window.location.href = "../view/explore.html";
   }
-funcion search(){
-  $.ajax({
-    var user = document.GetElementById("search_bar");
-    url: "/tw23-squisito/model/user_models/search_user.php",
-    type: "GET",
-    data: {
-      name: user,
-    },
-    success: function (result) {
-      const datiJSON = JSON.parse(result);
-       let postSpace = document.getElementById("postSpace");
-      for (var i = 0; i < datiJSON.length; i++) {
-        var cardCol = document.createElement("div");
-        cardCol.className = "col-sm-6 col-md-4 col-lg-3 item py-2";
-        cardCol.setAttribute("id", datiJSON[i].Username);
-
-        var cardImg = document.createElement("img");
-        cardImg.className = "img-fluid rounded";
-        cardImg.setAttribute(
-          "src",
-          "data:image/jpeg;base64," + datiJSON[i].FotoProfilo
-        );
-        cardCol.appendChild(cardImg);
-        cardCol.addEventListener("click", postClick, false);
-        postSpace.appendChild(cardCol);
-      }
-    },
+  $("#search_bar").change(function () {
+    var user = $("#search_bar").val();
+    $("#postSpace").empty();
+    if (user) {
+      $.ajax({
+        url: "/tw23-squisito/model/user_models/search_user.php",
+        type: "GET",
+        data: {
+          name: user,
+        },
+        success: function (result) {
+          if (result) {
+            const datiJSON = JSON.parse(result);
+            let postSpace = document.getElementById("postSpace");
+            for (var i = 0; i < datiJSON.length; i++) {
+              var cardCol = document.createElement("div");
+              cardCol.className = "col-sm-6 col-md-4 col-lg-3 item py-2";
+              cardCol.setAttribute("id", datiJSON[i].Username);
+              var cardImg = document.createElement("img");
+              cardImg.className = "img-fluid rounded";
+              cardImg.setAttribute(
+                "src",
+                "data:image/jpeg;base64," + datiJSON[i].FotoProfilo
+              );
+              cardCol.appendChild(cardImg);
+              postSpace.appendChild(cardCol);
+            }
+          }
+        },
+      });
+    }
   });
-
-}
