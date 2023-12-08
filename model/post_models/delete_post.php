c1a9582dfd7b28e3e983331259296cfec34ec10b
MARCO GIORGI <marco.giorgi7@studio.unibo.it>
2023-12-08 17:08:05 +0100

    Update delete_post.php fix sql error foreign key

--- model/post_models/delete_post.php
+++ model/post_models/delete_post.php
@@ -6,14 +6,48 @@
 include '../login_models/login_functions.php';
 require_once('../connection_models/db_conn.php');
 
-$query = "DELETE FROM post
+$query4 = "DELETE FROM post
         WHERE IDPost = ?";
+		
+$query = "DELETE FROM notifiche
+        WHERE IDPost = ?";
+		
+$query2 = "DELETE FROM mi_piace
+        WHERE IDPost = ?";
+		
+$query3 = "DELETE FROM commenti
+        WHERE IDPost = ?";		
 
 session_start();
 if (checkLogin($conn)) {
     if ($insert = $conn->prepare($query)) {
         $insert->bind_param('i', $_POST['IDPost']);
         if ($insert->execute()) {
+			if ($insert2 = $conn->prepare($query2)) {
+				$insert2->bind_param('i', $_POST['IDPost']);
+				if ($insert2->execute()) {
+					echo json_encode(array("success" => true));
+				} else {
+					echo json_encode(array("error" => $insert->error));
+				}
+			}
+            echo json_encode(array("success" => true));
+        } else {
+            echo json_encode(array("error" => $insert->error));
+        }
+    }
+	
+	if ($insert3 = $conn->prepare($query3)) {
+        $insert3->bind_param('i', $_POST['IDPost']);
+        if ($insert3->execute()) {
+			if ($insert4 = $conn->prepare($query4)) {
+				$insert4->bind_param('i', $_POST['IDPost']);
+				if ($insert4->execute()) {
+					echo json_encode(array("success" => true));
+				} else {
+					echo json_encode(array("error" => $insert->error));
+				}
+			}
             echo json_encode(array("success" => true));
         } else {
             echo json_encode(array("error" => $insert->error));
